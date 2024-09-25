4cdaa78d79b0f75f6192226919449e049ad57805
Pauloglhr <pauloglhr@outlook.com>
2024-09-25 14:15:22 -0300

    desenvolvida logica crud base

---
+++ src/script.js
@@ -0,0 +1,26 @@
+const getLocalStorage = () => 
+    JSON.parse(localStorage.getItem('db_client')) || [];
+const setLocalStorage = (client) => 
+    localStorage.setItem('db_client', JSON.stringify(client));
+
+//CRUD
+
+const deleteClient = (index) => {
+    const storage = readStorage();
+    storage.splice(index, 1);
+    setLocalStorage(storage);
+}
+
+const updateClient = (client, index) => {
+    const storage = readStorage();
+    storage[index] = client;
+    setLocalStorage(storage);
+}
+
+const readStorage = () => getLocalStorage();
+
+const createClient = (client) => {
+    const storage = getLocalStorage();
+    storage.push(client);
+    setLocalStorage(storage);
+}
